7fafcaba93a7e29239b76567292cf43f9cb02d5c
cjkk007788 <cjkk007788@naver.com>
2025-06-07 18:45:30 +0900

    commit

--- app/src/main/java/com/example/mobile_project_3/MainActivity.kt
+++ app/src/main/java/com/example/mobile_project_3/MainActivity.kt
@@ -4,16 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.mobile_project_3.ui.theme.Mobile_project_3Theme
-import com.example.mobile_project_3.ui_screen.MainScreen
-import androidx.navigation.compose.rememberNavController
 import com.example.mobile_project_3.Navigation.EasyWayNavGraph
-import com.example.mobile_project_3.ui.theme.Mobile_project_3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

69f2eba507f3e4fa0ae0e538f24070a1ad40bd9e
Sueon Park <162545677+sueon00@users.noreply.github.com>
2025-06-07 10:15:14 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -50,7 +51,15 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.material3)
+    implementation(libs.ui)
+    implementation(libs.androidx.material.icons.extended)
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation(libs.androidx.compose.material3.material3)
     implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
